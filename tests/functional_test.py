86b772c6ab164e8e6e8e325c9c97d3c17419cf6a
corrinerojas <crojas6@asu.edu>
2016-03-29 20:52:15 -0700

    Update functional_test.py

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -40,7 +40,7 @@ class TestFunctionalPointPattern(unittest.TestCase):
         """
         random.seed()  # Reset the random number generator using system time
         # I do not know where you have moved avarege_nearest_neighbor_distance, so update the point_pattern module
-        observed_avg = utils.average_nearest_neighbor_distance(self.points)
+        observed_avg = analytics.average_nearest_neighbor_distance(self.points)
         self.assertAlmostEqual(0.027, observed_avg, 3)
 
         # Again, update the point_pattern module name for where you have placed the point_pattern module
